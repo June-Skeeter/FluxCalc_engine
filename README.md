9d1ac5db2b8800cafe70c94832ebc94fe7f7233a
june-skeeter <thruhikerflash@gmail.com>
2025-03-27 13:06:28 -0700

    r2g

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 Toolset for modifying my custom fork of the eddypro-engine
 
+* To clone with the relevant submodules:
+
 `git clone --recurse-submodules  https://github.com/June-Skeeter/FluxCalc_engine.git`
+
+## Setup
+
+This is straightforward on linux.  Installing fortran on windows is more of a PITA.  It can be done, but I won't provide instruction here:
+
+* To setup the development environment:
+
+`sudo apt-get update`
+
+`sudo apt-get make`
+
+`sudo apt-get install gfortran`
+
+* To run default settings:
+
+`bash runEPDebug.bat`
+
+
+
--- runEPDebug.sh
+++ runEPDebug.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
-cd ./eddypro-engine/prj
+# !/bin/bash
+mypath=$(pwd)
+inipath=$mypath$"/custom/BinTest_linux.eddypro"
+rppath=$mypath$"/eddypro-engine/bin/linux/eddypro_rp"
+
+if [ -d ./custom/binOut ]; then
+    rm -rf custom/binOut 
+    mkdir custom/binOut
+else
+    mkdir custom/binOut
+fi
+
+if [ -d ./tmp ]; then
+    rm -rf tmp 
+fi
+mkdir tmp
+
+cd $mypath$"/eddypro-engine/prj"
 rm -rf ../bin ../obj
 make rp
-cd ../../
-./eddypro-engine/bin/linux/eddypro_rp -s linux /mnt/c/Users/User/EP_API_Test/custom/BinTest_linux.eddypro
+cd ../
+$rppath -s linux $inipath
+
