7f21569a3d795ac898f61c416b8097f22dcf96c4
June-Skeeter <thruhikerflash@gmail.com>
2025-03-27 14:43:36 -0700

    prep for change

--- runEPDebug.sh
+++ runEPDebug.sh
@@ -1,26 +1,30 @@
 # !/bin/bash
-mypath=$(pwd)
-inipath=$mypath$"/custom/BinTest_linux_ghg.eddypro"
-rppath=$mypath$"/eddypro-engine/bin/linux/eddypro_rp"
 
-if [ -d ./custom/binOut ]; then
-    rm -rf custom/binOut 
-    mkdir custom/binOut
-else
-    mkdir custom/binOut
-fi
+echo $0
+# mypath=$(pwd)
+# inipath=$mypath$"/custom/BinTest_linux_ghg.eddypro"
+# rppath=$mypath$"/eddypro-engine/bin/linux/eddypro_rp"
 
-if [ -d ./tmp ]; then
-    rm -rf tmp 
-fi
-mkdir tmp
+# if [ -d ./custom/binOut ]; then
+#     rm -rf custom/binOut 
+#     mkdir custom/binOut
+# else
+#     mkdir custom/binOut
+# fi
+
+# if [ -d ./tmp ]; then
+#     rm -rf tmp 
+# fi
+# mkdir tmp
 
 # cd $mypath$"/eddypro-engine/prj"
 # rm -rf ../bin ../obj
 # make rp
+# # make fcc
+# cp $mypath$/7za $mypath$"/eddypro-engine/prj/bin"
 # cd ../
-cd eddypro-engine
-$rppath -s linux $inipath
+# cd eddypro-engine
+# $rppath -s linux $inipath
 
 
 
