a30b1ff89cb9ab5626d422229601bf40e49ff9d4
june-skeeter <thruhikerflash@gmail.com>
2025-03-31 15:21:28 -0700

    functional float32 array example for SCL

--- runEPDebug.sh
+++ runEPDebug.sh
@@ -31,7 +31,7 @@ cd $rootdir$"/eddypro-engine"
 # $rootdir$"/eddypro-engine/bin/linux/eddypro_rp" -s linux $rootdir$"/custom/BinTest_linux.eddypro"
 
 
-$rootdir$"/eddypro-engine/bin/linux/eddypro_rp" -s linux $rootdir$"/custom/SCL.eddypro"
+$rootdir$"/eddypro-engine/bin/linux/eddypro_rp" -s linux $rootdir$"/custom/SCL_mod_mini.eddypro"
 
 
 
